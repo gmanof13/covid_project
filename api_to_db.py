024e76cab62c9441b4d39c4cdb306c02fa827bbd
Jordan <gmanof13@gmail.com>
2024-02-04 02:56:10 -0600

    created tables inside of local db. able to query from db

--- api_to_db.py
+++ api_to_db.py
@@ -7,8 +7,13 @@ import json
 from datetime import datetime, timedelta
 import os
 import shutil
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists,create_database
+import contextlib
+import sqlite3
 
 def create_json_dir(testing: bool = True):
+    """Creates JSON directory, if it doesn't exist."""
 
     if testing == True:
 
@@ -19,10 +24,12 @@ def create_json_dir(testing: bool = True):
             os.mkdir('json_dir')
     else:
 
-        pass 
+        None
 
 # todo: need to loop through querystring data field to append multiple JSONs by date. Then, at some point, check for missing dates???
 def api_to_json_dir():
+
+    """Pulls JSON into local directory"""
 
     url =  "https://covid-19-statistics.p.rapidapi.com/reports"
     
@@ -38,17 +45,37 @@ def api_to_json_dir():
 
     with open("json_dir/covid.json","w") as file:
         json.dump(data,file)
-    
-def create_db():
-    pass 
 
-def json_dir_to_db():
-    pass
+
+def create_db(testing: bool):
+
+    if testing == True:
+        conn = sqlite3.connect('covid.db')
+    else:
+        None
+
+
+def json_dir_to_db(db_name: str):
+
+    # read json into pandas df
+    with open("D:/python_projects/covid_project/json_dir/covid.json","r") as file:
+        data = json.load(file)
+    df = pd.json_normalize(data,record_path='data',max_level=1)
+    df = df.drop('region.cities', axis=1)
+    
+    # write pandas df into db
+    conn = sqlite3.connect('covid.db')
+    df.to_sql(f'{db_name}', conn, if_exists='replace')
+
 
 def main():
 
-    create_json_dir(testing=False)
-    api_to_json_dir()
+    # create_json_dir(testing=False)
+    # api_to_json_dir()
+    # create_db(testing=True)
+    json_dir_to_db(db_name='covid_tbl')
+
+
 
 if __name__ == "__main__":
     main()
--- db_testing.py
+++ db_testing.py
@@ -1,3 +1,30 @@
-import sqlalchemy
+import sqlite3
+import pandas as pd
 
-# check for missing dates
+def test_db_pandas(table: str):
+    conn = sqlite3.connect('covid.db')
+    df = pd.read_sql(f'select * from {table}',conn)
+    return df
+    conn.close()
+
+
+def show_all_tables():
+    
+    con = sqlite3.connect('covid.db')
+    cur = con.cursor()
+    cur.execute('''
+                
+        SELECT tbl_name
+        FROM sqlite_master
+        WHERE type = 'table';
+                
+    ''')
+
+    return cur.fetchall()
+
+df_test = test_db_pandas(table='covid_tbl')
+
+show_tables_df = show_all_tables()
+
+print(show_tables_df)
+
