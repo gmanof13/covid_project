4ac647fa387e6d976c199dde15ad7f4e7df33495
Jordan <gmanof13@gmail.com>
2024-02-08 15:54:27 -0600

    time series graph added

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,3 @@
-import sqlite3
 import streamlit as st 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,15 +6,13 @@ from sqlalchemy import create_engine
 engine = create_engine('postgresql+psycopg2://postgres:admin@localhost:5432/covid')
 
 
+def create_line_graph():
 
-def main():
-    
-    df = pd.read_sql_query('select * from "covid_tbl"',con=engine)
-
-
-    print(df.info())
-
-    
+    df = pd.read_sql(con=engine,sql="select * from us_deaths_time_series_tbl")
+    df = df[['date','sum_deaths']]
+    df['date'] = pd.to_datetime(df['date'])
+    st.line_chart(df,x='date',y='sum_deaths')
+    st.title("US Covid Deaths Over Time")
     
 if __name__ == '__main__':
-    main()
+    create_line_graph()
